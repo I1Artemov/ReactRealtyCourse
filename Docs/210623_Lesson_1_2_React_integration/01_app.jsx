efd7fa0b03400d37a904f052bc8e3ed39fff383f
Ivan <etherial.man@gmail.com>
2021-06-25 14:40:07 +0500

    Getting UseWebpackDevMiddleware working

---
+++ Docs/210623_Lesson_1_2_React_integration/01_app.jsx
@@ -0,0 +1,12 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div style={{ textAlign: "center", marginTop: "100px" }}>
+                This site is using React
+            </div>
+        );
+    }
+};
---
+++ Docs/210623_Lesson_1_2_React_integration/02_index.jsx
@@ -0,0 +1,9 @@
+﻿import React from 'react';
+import { render } from 'react-dom';
+import App from './Containers/app.jsx';
+
+render
+(
+    <App />,
+    document.getElementById('content')
+);
