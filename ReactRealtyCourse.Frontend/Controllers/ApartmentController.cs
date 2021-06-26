439b52b9b72fe6cd64202d7803c84632c9c3c16a
Ivan <etherial.man@gmail.com>
2021-06-26 13:15:03 +0500

    Lesson 2-2 Adding api controllers with sample data

---
+++ ReactRealtyCourse.Frontend/Controllers/ApartmentController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReactRealtyCourse.Business.Models;
+using System;
+using System.Collections.Generic;
+
+namespace ReactRealtyCourse.Frontend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApartmentController : Controller
+    {
+        [Route("getall")]
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            Apartment firstApartment = new Apartment
+            {
+                Id = 1,
+                CreateionDateTime = DateTime.Now,
+                HouseId = 1,
+                Floor = 4,
+                Price = 4500000.0f,
+                LivingSpace = 43.2f,
+                RoomAmount = 2
+            };
+            Apartment secondApartment = new Apartment
+            {
+                Id = 2,
+                CreateionDateTime = DateTime.Now.AddDays(-4),
+                HouseId = 2,
+                Floor = 3,
+                Price = 7800000.0f,
+                LivingSpace = 56.8f,
+                RoomAmount = 3
+            };
+            List<Apartment> apartments = new List<Apartment> { firstApartment, secondApartment };
+
+            return Json(apartments);
+        }
+
+        [Route("get")]
+        [HttpGet]
+        public IActionResult GetSingle(int? id)
+        {
+            if (!id.HasValue)
+                return NotFound("Id not provided");
+
+            Apartment firstApartment = new Apartment
+            {
+                Id = 1,
+                CreateionDateTime = DateTime.Now,
+                HouseId = 1,
+                Floor = 4,
+                Price = 4500000.0f,
+                LivingSpace = 43.2f,
+                RoomAmount = 2
+            };
+
+            return Json(firstApartment);
+        }
+    }
+}
---
+++ ReactRealtyCourse.Frontend/Controllers/HouseController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReactRealtyCourse.Business.Models;
+using System;
+using System.Collections.Generic;
+
+namespace ReactRealtyCourse.Frontend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HouseController : Controller
+    {
+
+        [Route("getall")]
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            House firstHouse = new House {
+                Id = 1, CreateionDateTime = DateTime.Now, Address = "Unknown",
+                MaxFloor = 6, BuildYear = 1968, WallMaterial = "brick"
+            };
+            House secondHouse = new House {
+                Id = 2, CreateionDateTime = DateTime.Now.AddDays(-8), Address = "Moscow, 19, Pushkin st.",
+                MaxFloor = 9, BuildYear = 1985, WallMaterial = "concrete panel"
+            };
+            List<House> houses = new List<House> { firstHouse, secondHouse };
+
+            return Json(houses);
+        }
+
+        [Route("get")]
+        [HttpGet]
+        public IActionResult GetSingle(int? id)
+        {
+            if (!id.HasValue)
+                return NotFound("Id not provided");
+
+            House firstHouse = new House {
+                Id = id.Value, CreateionDateTime = DateTime.Now, Address = "Some addres",
+                MaxFloor = 6, BuildYear = 1968, WallMaterial = "brick"
+            };
+
+            return Json(firstHouse);
+        }
+    }
+}
