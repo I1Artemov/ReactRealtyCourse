9d5613f6405f11ede3010e7b5308eeda43804d4e
Ivans <etherial.man@gmial.com>
2021-06-28 11:08:30 +0500

    Lesson 3-2 Redux integration

---
+++ Docs/210628_Lesson_3_2_Redux_integration/01_houseReadReducer.jsx
@@ -0,0 +1,7 @@
+﻿const initialState = {
+    houseInfo: {id: 1, address: 'Some address', buildYear: 1986, wallMaterial: 'brick', maxFloor: 9},
+};
+
+export default function flat(state = initialState, action) {
+    return state;
+}
